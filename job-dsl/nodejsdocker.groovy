0a8317147eb59dd6507fdf5bb895afcd2fd90aca
whocaresname <evgeniya.zaporozhets@gmail.com>
2022-01-10 15:19:17 +0200

    fix dockerhub repo

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('whocaresname/docker-nodejs-demo')
+            repositoryName('whocaresname/docker-nodjs-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
